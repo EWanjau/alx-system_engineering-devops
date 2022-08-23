6739e9411da7f4e6be05ad6c858540c9b7488fbb
EWanjau <wamwanjau@gmail.com>
2022-08-23 23:11:51 +0300

    created a regex that filters logs to sepcific format

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# matches few etters in input
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.?)\]\s\[flags:(.?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
