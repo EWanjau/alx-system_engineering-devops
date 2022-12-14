9d7d6606bb624d985f935eaaeb96ae734e25f1d0
EWanjau <wamwanjau@gmail.com>
2022-12-14 19:41:21 +0300

    added a script that kills a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # script kills a process with a specific name
-exec { 'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'pkill killmenow':
+  command => 'pkill -f killmenow',
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
