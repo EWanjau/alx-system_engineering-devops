cd8aadb8905cb27acd275104d1cb72fb1f9324bb
EWanjau <wamwanjau@gmail.com>
2022-12-14 19:59:29 +0300

    removed spaces

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # using puppet to create a new file, set its permisions, user and specify the path
 file { 'school':
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
