8137d23beaad04594389db240b435fdc51f6c825
EWanjau <wamwanjau@gmail.com>
2022-12-14 19:57:23 +0300

    corrected spaces

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # using puppet to install a package and specify the version
-  package { 'flask':
-    ensure => '2.1.0',
-    source => 'pip3',
-  }
+package { 'flask':
+    ensure   =>  '2.1.0',
+    provider => 'pip3',
+}
